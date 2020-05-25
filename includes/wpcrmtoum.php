39d294a087e2b9d7350fab16db0f436c32bcccb6
Herb Miller <herb@bobbingwide.com>
2020-05-25 10:47:25 +0100

    Generalise the mapping back from UM to WP-CRM. Implement the query_extra_account_fields filter. Improve use of tracing

--- includes/wpcrmtoum.php
+++ includes/wpcrmtoum.php
@@ -37,32 +37,31 @@ function get_um_field_values( $um_field ) {
     return $um_field_values;
 }
 
-function map_um_to_wpcrm( $user_id, $um_field ) {
+/**
+ * Maps the UM values back to the WP-CRM values
+ * The UM values are also stored.
+ *
+ * @param $user_id
+ * @param $um_field
+ * @param $wpcrm_field
+ */
+
+function map_um_to_wpcrm( $user_id, $um_field, $wpcrm_field ) {
     $um_field_values = get_um_field_values( $um_field );
     $field = UM()->builtin()->get_specific_field($um_field);
-    bw_trace2( $field, 'field');
-    if ( $field['type'] === 'radio' ) {
-        $wpcrm_field = str_replace( '_um', '', $um_field);
-    } else {
-        $wpcrm_field = $um_field;
-    }
+    bw_trace2( $field, 'field', true, BW_TRACE_DEBUG );
     $wpcrm_field_definition = get_wpcrm_field_definition( $wpcrm_field );
 
     if ( $wpcrm_field_definition) {
         if ( $wpcrm_field_definition['input_type'] === 'checkbox' ) {
             map_wpcrm_options_from_serialised($user_id, $wpcrm_field_definition, $um_field_values);
-
         } elseif ( $wpcrm_field_definition['input_type'] === 'radio' ) {
             map_wpcrm_value_from_um( $user_id, $wpcrm_field, $wpcrm_field_definition, $field, $um_field_values );
-
-
-
         }
         update_user_meta( $user_id, $um_field, $um_field_values );
     } else {
-
+        // Either WP-CRM is not activated or the field is not defined. Should we report an error?
     }
-
 }
 
 function map_wpcrm_options_to_serialised( $id, $wpcrm_field_definition, $um_field ) {
@@ -76,11 +75,9 @@ function map_wpcrm_options_to_serialised( $id, $wpcrm_field_definition, $um_fiel
         }
         $index++;
     }
-
-    bw_trace2( $um_array, 'um_array', false );
+    bw_trace2( $um_array, 'um_array', false, BW_TRACE_VERBOSE );
     $result = update_user_meta( $id, $um_field['metakey'], $um_array );
-    bw_trace2( $result, 'result', false );
-    //gob();
+    bw_trace2( $result, 'result', false, BW_TRACE_VERBOSE );
 }
 
 /**
@@ -126,15 +123,13 @@ function map_wpcrm_value_to_serialised( $id, $wpcrm_field, $wpcrm_field_definiti
  * @param $field
  * @param $um_field_values
  */
-
 function map_wpcrm_value_from_um( $user_id, $wpcrm_field, $wpcrm_field_definition, $field, $um_field_values ) {
-    bw_trace2();
     $um_value = $um_field_values[0];
     $um_option = $field['options'][$um_value];
-    bw_trace2( $um_option, 'um_option', false );
+    bw_trace2( $um_option, 'um_option', false, BW_TRACE_VERBOSE );
     $flipped = array_flip( $wpcrm_field_definition['option_labels'] );
     $wpcrm_value = $flipped[ $um_option ];
-    bw_trace2( $wpcrm_value, 'wpcrm_value', false);
+    bw_trace2( $wpcrm_value, 'wpcrm_value', true, BW_TRACE_DEBUG );
     update_user_meta( $user_id, $wpcrm_field, $wpcrm_value );
 }
 
@@ -185,83 +180,78 @@ function get_wpcrm_field_definition( $wpcrm_field ) {
     return $field_definition;
 }
 
-function showUMExtraFields( $args ) {
-    $id = um_user('ID');
-    UM()->user()->set( $id );  // We do this to clear the cache
-
-    $wpcrm_user = wp_crm_get_user( $id );
-    bw_trace2( $wpcrm_user, 'wpcrm user');
-
-    $names = apply_filters( 'query_extra_account_fields', [] );
-    $fields = array();
-    foreach( $names as $key => $wpcrm_name ) {
-        if ( is_numeric( $key ) ) {
-            $name = $wpcrm_name;
-        } else {
-            $name = $key;
-        }
-        $fields[$name] = UM()->builtin()->get_specific_field($name);
-        map_wpcrm_to_um( $id, $wpcrm_name, $fields[$name]);
+/**
+ * Gets the UM field's metakey when it's different from the WP-CRM field
+ * This is required for radio buttons.
+ * The convention is that the UM field name is suffixed `_um`.
+ *
+ * @param integer|string $key when integer the key names are the same
+ * @param string $wpcrm_name the name of the WP-CRM field
+ * @return mixed
+ */
+function get_um_name( $key, $wpcrm_name ) {
+    if (is_numeric($key)) {
+        $name = $wpcrm_name;
+    } else {
+        $name = $key;
     }
-    bw_trace2( $fields, "fields-before" );
-    //map_wpcrm_to_um( $id, 'multiple_chalets', $fields['multiple_chalets_um'] );
+    return $name;
+}
 
-    //map_wpcrm_to_um( $id, 'user_category', $fields['user_category'] );
+/**
+ * Displays the additional user fields on the Account page
+ * Performs a mapping of field values from WP-CRM to UM
+ *
+ * @param $args
+ * @throws \Exception
+ */
 
-    //map_wpcrm_to_um( $id, 'lease_length', $fields['lease_length'] );
-    //map_wpcrm_to_um( $id, 'multiple_chalets', $fields['multiple_chalets'] );
+function showUMExtraFields( $args )
+{
+    $id = um_user('ID');
+    UM()->user()->set($id);  // We do this to clear the cache
 
+    // @TODO Why do we need this?
+    $wpcrm_user = wp_crm_get_user($id);
+    bw_trace2($wpcrm_user, 'wpcrm user', true, BW_TRACE_VERBOSE);
+
+    $names = apply_filters('query_extra_account_fields', []);
+    $fields = array();
+    foreach ($names as $key => $wpcrm_name) {
+        $name = get_um_name($key, $wpcrm_name);
+        $fields[$name] = UM()->builtin()->get_specific_field($name);
+        map_wpcrm_to_um($id, $wpcrm_name, $fields[$name]);
+    }
+
+    // @TODO Determine if applying this filter is this really necessary?
+    bw_trace2($fields, "fields-before", false, BW_TRACE_DEBUG);
     $fields = apply_filters('um_account_secure_fields', $fields, $id);
-    bw_trace2( $fields, "fields-after" );
-    $output = '';
-    foreach( $fields as $key => $data )
-        $output .= UM()->fields()->edit_field( $key, $data );
+    bw_trace2($fields, "fields-after", false, BW_TRACE_DEBUG);
 
+    $output = '';
+    foreach ($fields as $key => $data) {
+        $output .= UM()->fields()->edit_field($key, $data);
+    }
     echo $output;
 }
 
+/**
+ * Maps the updated fields back to WP-CRM
+ *
+ * It appears that the $changes array doesn't contain the fields we're expecting
+ * so we basically ignore that and determine the names of the fields using a filter.
+ *
+ * @param $changes
+ * @param $user_id
+ */
 
-
-function getUMFormData( $changes, $user_id)
-{
-    //gob();
+function getUMFormData( $changes, $user_id) {
     bw_trace2();
-    //return;
     $id = um_user('ID');
-    $names = array('user_category',
-        /*
-        ['user_category_option_chalet-owner',
-            'user_category_option_association-member',
-            'user_category_option_committee-member',
-            'user_category_option_task-group-team-lead',
-            'user_category_option_task-group-member',
-            'user_category_option_contractor'],
-        */
-        'chalet_no',
-        'multiple_chalets_um',
-        'lease_length',
-
-        /*
-        'other_chalet_numbers',
-        'lease_start_date',
-        'date_chalet_purchased',
-        'address',
-        'town',
-        'city_or_county',
-        'postcode',
-        'phone_number',
-        'mobile_number',
-        'lease_start_date',
-        'lease_length',
-        'subscription_status',
-        'communication_preference',
-        'membership_state',
-        */
-    );
-
-    foreach ($names as $name) {
-        map_um_to_wpcrm( $id, $name);
-        //update_user_meta($id, $name, $_POST[$name]);
+    $names = apply_filters( 'query_extra_account_fields', [] );
+    foreach( $names as $key => $wpcrm_name ) {
+        $name = get_um_name( $key, $wpcrm_name );
+        map_um_to_wpcrm($id, $name, $wpcrm_name);
     }
-};
+}
 
